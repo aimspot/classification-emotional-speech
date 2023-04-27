fbbbc4d4db0cf2baf18538ce35a8891524517857
aimspot <82668230+aimspot@users.noreply.github.com>
2023-04-27 16:27:27 +0300

    update

--- core/extract_features.py
+++ core/extract_features.py
@@ -2,6 +2,7 @@ import os
 import pandas as pd
 import numpy as np
 import librosa
+from tqdm import tqdm
 
 def noise(data):
     noise_amp = 0.035*np.random.uniform()*np.amax(data)
@@ -19,23 +20,7 @@ def pitch(data, sampling_rate, pitch_factor=0.7):
     return librosa.effects.pitch_shift(data, sr=sampling_rate, n_steps=pitch_factor)
 
 
-def get_features (path):
-    y, sr =librosa.load(path, duration=2.5, offset=0.6)
-    res1=feature_extractors(y, sr)
-    result=np.array(res1)
-    
-    noise_data=noise(y)
-    res2=feature_extractors(noise_data, sr)
-    result=np.vstack((result,res2))
-    
-    new_data=stretch(y)
-    data_stretch_pitch=pitch(new_data, sr)
-    res3=feature_extractors(data_stretch_pitch, sr)
-    result=np.vstack((result,res3))
-    return result
-
-
-def feature_extractors(data):
+def feature_extractors(data, sr):
     result=np.array([])
     zcr=np.mean(librosa.feature.zero_crossing_rate(y=data).T, axis=0)
     result=np.hstack((result,zcr))
@@ -52,7 +37,21 @@ def feature_extractors(data):
     
     mel = np.mean(librosa.feature.melspectrogram(y=data, sr=sr).T, axis=0)
     result = np.hstack((result, mel)) 
+    return result
+
+def get_features (path):
+    y, sr =librosa.load(path, duration=2.5, offset=0.6)
+    res1=feature_extractors(y, sr)
+    result=np.array(res1)
     
+    noise_data=noise(y)
+    res2=feature_extractors(noise_data, sr)
+    result=np.vstack((result,res2))
+    
+    new_data=stretch(y)
+    data_stretch_pitch=pitch(new_data, sr)
+    res3=feature_extractors(data_stretch_pitch, sr)
+    result=np.vstack((result,res3))
     return result
 
 
@@ -74,6 +73,18 @@ def main():
     path_df = pd.DataFrame(file_path, columns=['Path'])
     Ravdess_df = pd.concat([emotion_df, path_df], axis=1)
     Ravdess_df.Emotions.replace({1:'neutral', 2:'calm', 3:'happy', 4:'sad', 5:'angry', 6:'fear', 7:'disgust', 8:'surprise'}, inplace=True)
+
+    X, Y = [], []
+    for path, emotion in tqdm(zip(Ravdess_df.Path, Ravdess_df.Emotions)):
+        feature = get_features(path)
+        for ele in feature:
+            X.append(ele)
+            # appending emotion 3 times as we have made 3 augmentation techniques on each audio file.
+        Y.append(emotion)
+
+    Features=pd.DataFrame(X)
+    Features['Labels'] = Y
+    Features.to_csv('final_csv_actor.csv', index=False)
 
 
 if __name__ == "__main__":
